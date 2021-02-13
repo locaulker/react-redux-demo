7e2bd1b2a24a7b95bd9d56bf29625fb4cec9d00d
Lawrence O. Caulker <locaulker@gmail.com>
2021-02-13 02:52:27 -0500

    added redux store

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import logo from "./logo.svg"
+import { Provider } from "react-redux"
 import "./App.css"
 import CakeContainer from "./components/CakeContainer"
+import store from "./redux/store"
 
 function App() {
   return (
-    <div className="App">
-      <CakeContainer />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <CakeContainer />
+      </div>
+    </Provider>
   )
 }
 
