c495564cb3d4cbc1c8905b401e9c6a82b6860827
Lawrence O. Caulker <locaulker@gmail.com>
2021-02-13 09:31:44 -0500

    completed basic react-redux mini project

--- src/components/CakeContainer.js
+++ src/components/CakeContainer.js
@@ -1,12 +1,30 @@
 import React from "react"
+import { connect } from "react-redux"
+import { buyCake } from "../redux"
 
-function CakeContainer() {
+function CakeContainer(props) {
   return (
     <div>
-      <h2>Number of cakes</h2>
-      <button>Buy cake</button>
+      <h2>Number of cakes - {props.numOfCakes}</h2>
+      <button onClick={props.buyCake}>Buy Cake</button>
     </div>
   )
 }
 
-export default CakeContainer
+// step 1 - define selectors
+const mapStateToProps = (state) => {
+  return {
+    numOfCakes: state.numOfCakes,
+  }
+}
+
+// step 2 -
+const mapDispatchToProps = (dispatch) => {
+  return {
+    buyCake: () => dispatch(buyCake()),
+  }
+}
+
+// step 3 -
+
+export default connect(mapStateToProps, mapDispatchToProps)(CakeContainer)
