047e919794760d03332f26c04dd2cea63aec03eb
Lawrence O. Caulker <locaulker@gmail.com>
2021-02-13 12:02:01 -0500

    updated react hooks

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from "./logo.svg"
 import { Provider } from "react-redux"
 import "./App.css"
 import CakeContainer from "./components/CakeContainer"
+import HooksCakeContainer from "./components/HooksCakeContainer"
 import store from "./redux/store"
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <HooksCakeContainer />
         <CakeContainer />
       </div>
     </Provider>
